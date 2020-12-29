4ae7f6436fcaf0b113eb307195fffb2591542ebd
yn919 <nk19yu@gmail.com>
2020-12-29 18:23:26 +0900

    日付選択を実装

--- kins/lib/main.dart
+++ kins/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'schedule.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: null,
+      home: ScheduleScreen(),
     );
   }
 }
@@ -23,13 +27,80 @@ class MyApp extends StatelessWidget {
 class ScheduleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('予定表')),
-      body: null,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+    return ChangeNotifierProvider<ScheduleDatas>(
+      create: (_) => ScheduleDatas(),
+      child: Scaffold(
+          appBar: AppBar(title: Text('予定表')),
+          body: ScheduleList(),
+          floatingActionButton:
+              Consumer<ScheduleDatas>(builder: (_, scheduleDatas, __) {
+            return FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () => scheduleDatas.addSchedule(),
+            );
+          })),
+    );
+  }
+}
+
+class ScheduleList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _scheduleDatas = Provider.of<ScheduleDatas>(context);
+    return ListView.builder(
+      itemCount: _scheduleDatas.schedules.length,
+      itemBuilder: (context, index) => ChangeNotifierProvider.value(
+        value: _scheduleDatas.schedules[index],
+        child: ScheduleTile(),
       ),
     );
+  }
+}
+
+class ScheduleTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _scheduleData = Provider.of<ScheduleData>(context);
+    return Card(
+      child: Padding(
+        child: Row(
+          children: [DatePickerButton()],
+        ),
+        padding: EdgeInsets.all(10.0),
+      ),
+    );
+  }
+}
+
+class DatePickerButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _scheduleData = Provider.of<ScheduleData>(context);
+    return RaisedButton(
+      child: Row(
+        children: [
+          Icon(Icons.timelapse),
+          Text("${_getDayFormatString(_scheduleData.date)})")
+        ],
+      ),
+      onPressed: () => _selectDate(context, _scheduleData),
+    );
+  }
+
+  Future<Null> _selectDate(
+      BuildContext context, ScheduleData scheduleData) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: scheduleData.date,
+        firstDate: DateTime.now(),
+        lastDate: DateTime.now().add(Duration(days: 360)));
+    if (picked != null) {
+      scheduleData.date = picked;
+    }
+  }
+
+  String _getDayFormatString(DateTime date) {
+    initializeDateFormatting('ja');
+    return DateFormat('MM/dd(EEEE)').format(date).toString();
   }
 }
